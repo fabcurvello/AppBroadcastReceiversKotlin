bfea1fe53337ba780d0edabf8398613c8818ae9d
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-07-28 16:03:17 -0300

    MainActivity.kt concluída

--- app/src/main/java/br/com/fabriciocurvello/appbroadcastreceiverskotlin/MainActivity.kt
+++ app/src/main/java/br/com/fabriciocurvello/appbroadcastreceiverskotlin/MainActivity.kt
@@ -1,5 +1,7 @@
 package br.com.fabriciocurvello.appbroadcastreceiverskotlin
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +9,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private val networkChanceReceiver = NetworkChangeReceiver()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Registra o receptor dinamicamente
+        val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(networkChanceReceiver, filter)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Cancela o registro do receptor
+        unregisterReceiver(networkChanceReceiver);
     }
 }
